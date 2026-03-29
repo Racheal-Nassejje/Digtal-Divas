210addbe650a47101a67d18adc0e4557243fa748
Racheal-Nassejje <nassejjeracheal22@gmail.com>
2026-03-29 14:44:17 +0300

    Added contact, rates us, feedback and find us map sections

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -48,3 +48,15 @@ Total: UGX ${Number(total).toLocaleString()}`);
 
     document.getElementById('orderForm').reset();
 }
+// Rate Us Function
+function rateUs(rating) {
+    const messages = {
+        5: "Thank you! We're glad you loved Digtal-Divas Meals! 🎉",
+        4: "Thank you! We're happy you enjoyed our foods! 😊",
+        3: "Thank you! We'll keep improving! 💪",
+        2: "Thank you! We'll do better next time! 🙏",
+        1: "Thank you! We're sorry to disappoint! 😔"
+    }
+    document.getElementById('rating-message').innerHTML = 
+        `<strong style="color:#FFD700">${messages[rating]}</strong>`;
+}
